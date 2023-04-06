ac4b7d382f97cef8186729647a5c7e64b5c0fa57
Abdulameen <ifeoluwaabdulameen@gmail.com>
2023-04-06 17:26:10 +0100

    fixes

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-#Build the project
 echo "Building the project..."
-pip install Django==4.2
-
-echo "Building the project..."
-pip install -r requirements.txt
+pip install -r requirements.txt --use-deprecated=legacy-resolver
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.11.0 manage.py makemigrations --noinput
+python3.11.0 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.11.0 manage.py collectstatic --noinput --clear
