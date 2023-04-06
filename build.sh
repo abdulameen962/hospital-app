c0ee21ff0973afcfd84846660a7af095eafbaf02
Abdulameen <ifeoluwaabdulameen@gmail.com>
2023-04-06 23:07:37 +0100

    fixes

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@
 echo "Building the project..."
 pip install -r requirements.txt
 
+echo "Migrating project"
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput
