3d490430995fc4d0cc6c28d5fc69f4a3d1ff8477
Abdulameen <ifeoluwaabdulameen@gmail.com>
2023-04-06 17:54:54 +0100

    fixes

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ echo "Building the project..."
 pip install -r requirements.txt --use-deprecated=legacy-resolver
 
 echo "Make Migration..."
-python3.11.0 manage.py makemigrations --noinput
-python3.11.0 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.11.0 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
