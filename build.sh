dd087948aaf24b352cbedf088527e937febc71a2
Abdulameen <ifeoluwaabdulameen@gmail.com>
2023-04-06 17:08:31 +0100

    fixing build error

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 #Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+pip install Django==4.2
+
+echo "Building the project..."
+pip install -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
