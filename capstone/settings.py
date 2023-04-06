3023a1a35f0181305a5fc8c039ddede60b413394
Abdulameen <ifeoluwaabdulameen@gmail.com>
2023-04-06 03:36:18 +0100

    moving

--- capstone/settings.py
+++ capstone/settings.py
@@ -97,7 +97,10 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=os.environ.get('DATABASE_URL')),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_USER_MODEL = 'hospital.User'
